b68ac9d15bd0cbf758441d90cf4555d062f6cec7
cpc <cpc@bnext.es>
2021-02-04 10:55:17 +0100

    Fix data models

--- app/src/main/java/com/example/openbankmobiletest/model/CharacterDataContainer.kt
+++ app/src/main/java/com/example/openbankmobiletest/model/CharacterDataContainer.kt
@@ -5,6 +5,7 @@ data class CharacterDataContainer(
     val offset: Int,
     val limit: Int,
     val total: Int,
-    val count: Int
+    val count: Int,
+    val results: List<Character>
 
 )
--- app/src/main/java/com/example/openbankmobiletest/model/CharacterDataWrapper.kt
+++ app/src/main/java/com/example/openbankmobiletest/model/CharacterDataWrapper.kt
@@ -3,6 +3,7 @@ package com.example.openbankmobiletest.model
 data class CharacterDataWrapper (
 
     val code: Int,
-    val status: String
+    val status: String,
+    val data: CharacterDataContainer
 
 )
